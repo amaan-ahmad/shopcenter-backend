04e204e13bfa946d7628c5a280d2414d738510d3
Amaan Ahmad <amaanahmad582001@gmail.com>
2021-03-16 20:06:22 +0530

    added buyer model

---
+++ models/buyer.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const { ObjectId } = mongoose.Schema.Types;
+
+const BuyerSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cart: [
+    {
+      productId: { type: ObjectId, ref: "products" },
+    },
+  ],
+  wishlist: [
+    {
+      productId: { type: ObjectId, ref: "products" },
+    },
+  ],
+});
+
+module.exports = model("buyers", BuyerSchema);
--- models/index.js
+++ models/index.js
@@ -1,9 +1,10 @@
 const category = require("./category");
 const products = require("./products");
 const seller = require("./seller");
-
+const buyer = require("./buyer");
 module.exports = {
   category,
   products,
   seller,
+  buyer,
 };
